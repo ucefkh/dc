ff8880fcc9b9579d6a6f9ef9f01cdda86de619ef
Arsalan Bashir <mail@arsalanbashir.com>
2014-05-27 21:28:48 +0530

    Functional login and basic profiles, in alpha

--- dc/views.py
+++ dc/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, session
 from dc import dc, db, models
 from forms import SignupForm
 from models import User
@@ -14,5 +14,23 @@ def test():
 
 @dc.route("/signup", methods=['GET', 'POST'])
 def signup():
-	form = SignupForm()
-	return render_template("signup.html", form = form)
+    form = SignupForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if form.password.data == form.repassword.data:
+                item = models.User(fullname = form.name.data, email = form.email.data, password = form.password.data)
+                db.session.add(item)
+                db.session.commit()
+
+                session['email'] = item.email
+                return render_template('profile.html', email = session['email'])
+
+            else:
+                return "Password Mismatch"
+
+        else:
+            form.email.errors.append("Error")
+            return render_template('signup.html', form = form)
+    
+    elif request.method == 'GET':
+        return render_template('signup.html', form = form)
