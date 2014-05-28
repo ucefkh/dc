f90915b0913c6125f0814c3590181fb6222b99d5
Arsalan Bashir <mail@arsalanbashir.com>
2014-05-28 15:24:11 +0530

    Worked on helper functions

--- dc/chef.py
+++ dc/chef.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, url_for, request, session
 from dc import dc, db, models
 from forms import SignupForm, LoginForm
 from models import User
+from sessions import SignUp, SignIn
 import time
 
 
@@ -17,8 +18,7 @@ def test():
 def signup():
     form = SignupForm()
     if request.method == 'POST':
-        
-    
+        SignUp(form, form.name.data, form.email.data, form.password.data)    
     elif request.method == 'GET':
         return render_template('signup.html', form = form)
 
@@ -26,18 +26,6 @@ def signup():
 def login():
     form = LoginForm()
     if request.method == 'POST':
-        if form.email.data.lower() not in session:
-            if form.validate_on_submit():
-                user = User.query.filter_by(email = form.email.data.lower()).first()            
-                if user and user.check_password(form.password.data):
-                    session['email'] = user.email
-
-                    user.lastseen = time.strftime("%Y-%m-%d")
-                    db.session.commit()
-
-                    return render_template("profile.html", email = session['email'])
-                else:
-                    form.email.errors.append("Invalid e-mail or password")
-                    return render_template('login.html', form=form)            
+        SignIn(form, form.email.data, form.password.data)           
     elif request.method == 'GET':
         return render_template('login.html', form=form)
--- dc/sessions.py
+++ dc/sessions.py
@@ -1,14 +1,16 @@
-# Make Imports Here
+from dc import models, db
+from models import User
+from flask import session, render_template
 
-def SignUp(self, form, name, email, password):
-	if form.validate_on_submit():
+def SignUp(form, name, email, password):
+    if form.validate_on_submit():
         if form.password.data == form.repassword.data:
-            item = models.User(fullname = form.name.data, email = form.email.data, password = form.password.data)
-            db.session.add(item)
+            user = models.User(fullname = name, email = email, password = password)
+            db.session.add(user)
             db.session.commit()
 
-            session['email'] = item.email
-            return render_template('profile.html', email = session['email'])
+            session['email'] = user.email
+            return render_template('profile.html', email = session['email']) 
 
         else:
             return "Password Mismatch"
@@ -17,8 +19,20 @@ def SignUp(self, form, name, email, password):
         form.email.errors.append("Error")
         return render_template('signup.html', form = form)
 
-def SignIn():
-	pass
+def SignIn(form, email, password):
+    if form.email.data not in session:
+        if form.validate_on_submit():
+            user = User.query.filter_by(email = form.email.data.lower()).first()                
+            if user and user.check_password(form.password.data):
+                session['email'] = user.email
+
+                user.lastseen = time.strftime("%Y-%m-%d")
+                db.session.commit()
+
+                return render_template("profile.html", email = session['email'])
+            else:
+                form.email.errors.append("Invalid e-mail or password")
+                return render_template('login.html', form = form)
 
 def DeleteUser():
 	
