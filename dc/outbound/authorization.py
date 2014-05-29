e36e74698e75370bf04b80d0676805f906e42957
Arsalan Bashir <mail@arsalanbashir.com>
2014-05-29 17:35:02 +0530

    Added authorization script

--- dc/outbound/authorization.py
+++ dc/outbound/authorization.py
@@ -1,1 +1,10 @@
-test_message = "Works1"
+from flask.ext.mail import Message, Mail
+
+dc.config['MAIL_SERVER']='smtp.gmail.com'
+dc.config['MAIL_PORT'] = 465
+dc.config['MAIL_USERNAME'] = 'dev.dreamscollective@gmail.com'
+dc.config['MAIL_PASSWORD'] = 'xx;'		# 	You wish, loser!
+dc.config['MAIL_USE_TLS'] = False
+dc.config['MAIL_USE_SSL'] = True
+
+mail.init_app(dc)
