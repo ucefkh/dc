23d1fb871badb8ebaa8a41c09a207b2ca2055721
Arsalan Bashir <mail@arsalanbashir.com>
2014-05-29 17:39:58 +0530

    Added basic structure for email handling

--- dc/outbound/mailhandler.py
+++ dc/outbound/mailhandler.py
@@ -4,6 +4,20 @@ send scripts
 use mailchimp API 
 
 receive and send auto responses
+
 """
 
-test_handler = "this too"
+recipients = []
+cause = []
+
+def MakeContent(template, cause):
+	pass
+
+# Synthesis? Hmm. Backlogged.
+
+def SendMail(content, recipients):
+	msg = Message(form.subject.data, sender='dev.dreamscollective@gmail.com', recipients=recipients)
+	msg.body = MakeContent(template, cause)
+	mail.send(msg)
+	
+	# return render_template('contact.html')
