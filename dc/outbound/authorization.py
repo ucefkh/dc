e730aaca25e1134a51fd8929cabde14ffadd1b4c
Arsalan Bashir <mail@arsalanbashir.com>
2014-06-04 22:31:30 +0530

    Auth

--- dc/outbound/authorization.py
+++ dc/outbound/authorization.py
@@ -1,6 +1,6 @@
 MAIL_SERVER ='smtp.gmail.com'
 MAIL_PORT  = 465
 MAIL_USERNAME  = 'dev.dreamscollective@gmail.com'
-MAIL_PASSWORD  = '1Q2W3e4r_'		# 	You wish, loser!
+MAIL_PASSWORD  = ''		
 MAIL_USE_TLS  = False
 MAIL_USE_SSL  = True
