c65f1ae2fed637843cd83f3ea0ab042e27d64454
Arsalan Bashir <mail@arsalanbashir.com>
2014-06-09 17:12:50 +0530

    Modified mailhandler to receive email

--- dc/outbound/mailhandler.py
+++ dc/outbound/mailhandler.py
@@ -1,6 +1,13 @@
 import smtplib
 import authorization
 
+MAIL_SERVER ='smtp.gmail.com'
+MAIL_PORT  = 465
+MAIL_USERNAME  = 'dev.dreamscollective@gmail.com'
+MAIL_PASSWORD  = '1Q2W3e4r_'   
+MAIL_USE_TLS  = False
+MAIL_USE_SSL  = True
+
 def MakeContent(template):
 	return """
 
@@ -22,27 +29,24 @@ def SendMail(template, recipients):
     body = MakeContent(template)
     subject = "Title"
 
-    
-
-    """Sends an e-mail to the specified recipient."""
-     
-    body = "" + body + ""
-     
-    headers = ["From: " + MAIL_USERNAME,
-               "Subject: " + subject,
-               "To: " + recipient,
-               "MIME-Version: 1.0",
-               "Content-Type: text/html"]
-    headers = "\r\n".join(headers)
-     
-    session = smtplib.SMTP(MAIL_SERVER, MAIL_PORT)
-     
-    session.ehlo()
-    session.starttls()
-    session.ehlo
-    session.login(MAIL_USERNAME, MAIL_PASSWORD)
-     
-    session.sendmail(MAIL_USERNAME, recipient, headers + "\r\n\r\n" + body)
-    session.quit()
-	
-	 return render_template('contact.html')
+    for recipient in recipients:
+      """Sends an e-mail to the specified recipient."""
+       
+      body = "" + body + ""
+       
+      headers = ["From: " + MAIL_USERNAME,
+                 "Subject: " + subject,
+                 "To: " + recipient,
+                 "MIME-Version: 1.0",
+                 "Content-Type: text/html"]
+      headers = "\r\n".join(headers)
+       
+      session = smtplib.SMTP(MAIL_SERVER, MAIL_PORT)
+       
+      session.ehlo()
+      session.starttls()
+      session.ehlo
+      session.login(MAIL_USERNAME, MAIL_PASSWORD)
+       
+      session.sendmail(MAIL_USERNAME, recipient, headers + "\r\n\r\n" + body)
+      session.quit()
