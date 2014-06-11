941815842091c06b68466663ea6c91799235d9da
Arsalan Bashir <mail@arsalanbashir.com>
2014-06-11 21:46:26 +0530

    Added Boto for AWS hookups ;)

--- dc/chef.py
+++ dc/chef.py
@@ -30,6 +30,23 @@ def test_mail():
 
     return "Sent"
 
+@dc.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        rec = Photo(filename=filename, user=g.user.id)
+        rec.store()
+        flash("Photo saved.")
+        return redirect(url_for('show', id=rec.id))
+    return render_template('upload.html')
+
+@dc.route('/photo/<id>')
+def show(id):
+    photo = Photo.load(id)
+    if photo is None:
+        abort(404)
+    url = photos.url(photo.filename)
+    return render_template('show.html', url=url, photo=photo)
 
 ##############
 
